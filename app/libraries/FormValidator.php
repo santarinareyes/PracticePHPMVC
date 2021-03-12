afd04273e43fa5b8a33a4f09b95da032746ffd39
Richard <richard.santarinareyes@medieinstitutet.se>
2021-03-12 09:54:07 +0100

    learning

--- app/libraries/FormValidator.php
+++ app/libraries/FormValidator.php
@@ -5,11 +5,11 @@
             
             if(strlen($string) < 2){
                 $error = Constants::$stringMin;
+                return "<span class='error_message'>$error</span>";
             } else if (strlen($string) > 25){
                 $error = Constants::$stringMax;
+                return "<span class='error_message'>$error</span>";
             }
-
-            return "<span class='error_message'>$error</span>";
         }
 
         public static function validatePassword($string){
@@ -17,9 +17,8 @@
             
             if(strlen($string) < 6){
                 $error = Constants::$passwordMin;
+                return "<span class='error_message'>$error</span>";
             }
-
-            return "<span class='error_message'>$error</span>";
         }
 
         public static function validateConfirmPassword($string1, $string2){
@@ -27,11 +26,11 @@
             
             if(strlen($string1) < 6){
                 $error = Constants::$passwordMin;
+                return "<span class='error_message'>$error</span>";
             } else if ($string1 !== $string2){
                 $error = Constants::$passwordDontMatch;
+                return "<span class='error_message'>$error</span>";
             }
-
-            return "<span class='error_message'>$error</span>";
         }
 
         public static function validateEmails($string1, $string2){
@@ -39,12 +38,12 @@
             
             if(!filter_var($string1, FILTER_VALIDATE_EMAIL)){
                 $error = Constants::$emailInvalid;
+                return "<span class='error_message'>$error</span>";
             }
 
             if ($string1 !== $string2){
                 $error = Constants::$emailDontMatch;
+                return "<span class='error_message'>$error</span>";
             }
-
-            return "<span class='error_message'>$error</span>";
         }
     }
