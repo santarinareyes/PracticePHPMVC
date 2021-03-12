acd50eb1b8b3d89e308c78c53861a59c5dc1a505
Richard <richard.santarinareyes@medieinstitutet.se>
2021-03-12 16:53:59 +0100

    some styling

--- app/views/start/index.php
+++ app/views/start/index.php
@@ -3,6 +3,12 @@
     <video class="preview_video" src="<?php echo $data["entity"]->entity_preview; ?>" autoplay muted></video>
 
     <div class="preview_overlay">
-        test
+        <div class="main_details">
+            <h3><?php echo $data["entity"]->entity_name; ?></h3>
+            <div class="buttons">
+                <button><i class="fas fa-play"></i> Play</button>
+                <button><i class="fas fa-volume-mute"></i></button>
+            </div>
+        </div>
     </div>
 </div>
