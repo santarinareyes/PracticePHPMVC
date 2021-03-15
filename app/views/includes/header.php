5b776ecb78325d6de13f640494660d017e90534f
Richard <richard.santarinareyes@medieinstitutet.se>
2021-03-15 12:53:04 +0100

    solution for not showing main nav bar in specific cases

--- app/views/includes/header.php
+++ app/views/includes/header.php
@@ -12,19 +12,3 @@
 </head>
 <body>
     <div class="wrapper">
-        <div class="main_nav-bar">
-            <div class="logo-container">
-                <a href="<?php echo URLROOT;?>">
-                    <img src="<?php echo URLROOT; ?>/images/richflix_logo.png" alt="richflix-logo">
-                </a>
-            </div>
-            <ul class="nav-links">
-                <li><a href="<?php echo URLROOT;?>">Home</a></li>
-                <li><a href="<?php echo URLROOT;?>">TV Shows</a></li>
-                <li><a href="<?php echo URLROOT;?>">Movies</a></li>
-            </ul>
-            <div class="right-nav-items">
-                <a href=""><i class="fas fa-search"></i></a>
-                <a href=""><i class="fas fa-user"></i></a>
-            </div>
-        </div>
--- public/index.php
+++ public/index.php
@@ -1,5 +1,8 @@
 <?php 
     require_once "../app/bootstrap.php";
     require APPROOT . "/views/includes/header.php";
+    if(!str_contains($_GET["url"], "watch")):
+    require APPROOT . "/views/includes/nav.php";
+    endif;
     $init = new App();
     require APPROOT . "/views/includes/footer.php";
